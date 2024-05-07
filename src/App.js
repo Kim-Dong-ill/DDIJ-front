ec49c2207a2ccbdd9baa0408450f3a42a72ff99c
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-05-07 18:02:27 +0900

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/tStyle.css";
+import "./assets/index.scss";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App
+      <div className="nanum" style={{ fontSize: "100px", fontWeight: [100] }}>
+        s나눔고딕
+      </div>
+      <div style={{ fontSize: "100px" }}>s나꿱뷁뉅눔고딕</div>
+      <div style={{ fontSize: "12px" }}>s나꿱뷁눔고딕 12px</div>
+      <div style={{ fontSize: "16px" }}>s나꿱뷁눔고딕 16px</div>
+      <div style={{ fontSize: "20px" }}>s나꿱뷁눔고딕 20px</div>
+      <div style={{ fontSize: "22px" }}>s나꿱뷁눔고딕 22px</div>
+      <div style={{ fontSize: "26px" }}>s나꿱뷁눔고딕 26px</div>
+      <div style={{ fontSize: "30px" }}>s나꿱뷁눔고딕 30px</div>
+      <div style={{ fontSize: "40px" }}>s나꿱뷁눔고딕 40px</div>
+      <div className="nanum" style={{ fontSize: "12px" }}>
+        s나꿱뷁눔고딕 12px
+      </div>
+      <div className="nanum" style={{ fontSize: "16px" }}>
+        s나꿱뷁눔고딕 16px
+      </div>
+      <div className="nanum" style={{ fontSize: "20px" }}>
+        s나꿱뷁눔고딕 20px
+      </div>
+      <div className="nanum" style={{ fontSize: "22px" }}>
+        s나꿱뷁눔고딕 22px
+      </div>
+      <div className="nanum text-ye-400" style={{ fontSize: "26px" }}>
+        s나꿱뷁눔고딕 26px
+      </div>
+      <div className="nanum bg-da-100" style={{ fontSize: "30px" }}>
+        s나꿱뷁눔고딕 30px
+      </div>
+      <div className="nanum bg-ye-400" style={{ fontSize: "40px" }}>
+        s나꿱뷁눔고딕 40px
+      </div>
     </div>
   );
 }
