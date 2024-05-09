ab7c818c35b49d2fc37ef91cd1c1bfe32e08d350
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-05-09 17:22:04 +0900

    2024-05-09

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(props) {
   return (
     <>
       <div className="flex items-end justify-between h-[90px] px-[10px]">
-        <h1 className="text-white navbar">{props}</h1>
+        <h1 className="text-white navbar">{props.title}</h1>
         <Link to="/">
           <img src="/images/DDIJlogo.svg" alt="" />
         </Link>
