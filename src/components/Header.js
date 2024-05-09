b8ee6817d5a54f1d8e2d76b5ec3b8b8464443e96
jjh <wogml220723@naver.com>
2024-05-09 17:22:31 +0900

    20240509

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Header(props) {
+function Header() {
   return (
     <>
       <div className="flex items-end justify-between h-[90px] px-[10px]">
-        <h1 className="text-white navbar">{props}</h1>
+        <h1 className="text-white navbar"></h1>
         <Link to="/">
           <img src="/images/DDIJlogo.svg" alt="" />
         </Link>
