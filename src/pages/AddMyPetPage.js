743817b6c7437c3f1521991b97dbaf5ed5d04b23
jjh <wogml220723@naver.com>
2024-05-13 17:54:33 +0900

    Petttttttt

--- src/pages/AddMyPetPage.js
+++ src/pages/AddMyPetPage.js
@@ -1,10 +1,119 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import ButtonBl from "../components/ButtonBl";
+import ButtonYe from "../components/ButtonYe";
 
 function AddMyPetPage() {
   return (
     <div>
-      AddMyPetPage
+      {/* subheader start */}
+      <div className="subHeader relative bg-ye-600 w-[500px] top-0 fixed h-[260px] text-center">
+        <div className="h-[50px] border-b-2 mb-3 flex  justify-between items-center">
+          <h2>
+            <img src="/images/backicon.svg" alt="" />
+          </h2>
+          <h2>반려견 추가</h2>
+          <h2 className="invisible">
+            <img src="/images/backicon.svg" alt="" />
+          </h2>
+        </div>
+
+        {/* name start */}
+        <div className="grid gap-3">
+          <div className=" bg-ye-500 h-[100px] w-[100px] rounded-full flex justify-center items-center m-auto ">
+            <img
+              src="/images/camera1.svg"
+              className="w-[60px] h-[60px] m-auto"
+            />
+          </div>
+
+          {/* button start */}
+          <div className="flex justify-center">
+            <button className="border border-da-300 px-[5px] py-[3px] rounded-lg">
+              <span className="inline-block text-[13px] mr-[3px]">
+                사진추가
+              </span>
+              {/* <img src="./images/plus.svg" className="w-[14px] h-[14px]" /> */}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white" style={{ height: "calc(100vh - 65px)" }}>
+        {/* view start*/}
+        <div className="w-[400px]  m-auto grid gap-3 pt-[50px] pb-[115px] ">
+          {/* <div className=" flex-col gap-y-20 "> */}
+
+          {/* input start */}
+          <div className="w-[400px] nanum flex-col items-center space-y-10 mb-[50px]">
+            <div className="p-2">
+              <h2 className="nanumBold p-[10px]">이름</h2>
+              <input
+                type="text"
+                className="w-full p-[10px] nanum border-b-2 border-da-100"
+              ></input>
+            </div>
+
+            <div className="p-2">
+              <h2 className="nanumBold p-[10px]">나이</h2>
+              <input
+                type="text"
+                className="w-full p-[10px] nanum border-b-2 border-da-100 "
+              ></input>
+            </div>
+
+            <div className="p-2">
+              <h2 className="nanumBold p-[10px]">성별</h2>
+              <input
+                type="text"
+                className="w-full p-[10px] nanum border-b-2 border-da-100"
+              ></input>
+            </div>
+
+            <div className="p-2">
+              <p className="nanumBold p-[10px] ">중성화여부</p>
+              <div className="flex px-[10px]">
+                <button className="inline-block flex-auto w-[50%] mr-[5px] px-[5px] py-[5px] border border-gray-200 rounded-md hover:bg-da-300 hover:text-white">
+                  했어요
+                </button>
+                <button className="inline-block flex-auto w-[50%] px-[5px] py-[5px] border border-gray-200 rounded-md hover:bg-da-300 hover:text-white">
+                  안했어요
+                </button>
+              </div>
+            </div>
+
+            <div className="p-2">
+              <h2 className="nanumBold p-[10px]">기본접종</h2>
+              <div className="flex p-[10px]">
+                <button className="inline-block flex-auto w-[50%] mr-[5px] px-[5px] py-[5px] border border-gray-200 rounded-md hover:bg-da-300 hover:text-white">
+                  했어요
+                </button>
+                <button className="inline-block flex-auto w-[50%] px-[5px] py-[5px] border border-gray-200 rounded-md hover:bg-da-300 hover:text-white">
+                  안했어요
+                </button>
+              </div>
+            </div>
+
+            <div className="p-2">
+              <h2 className="nanumBold p-[10px]">광견병</h2>
+              <div className="flex p-[10px]">
+                <button className="inline-block flex-auto w-[50%] mr-[5px] px-[5px] py-[5px] border border-gray-200 rounded-md hover:bg-da-300 hover:text-white">
+                  했어요
+                </button>
+                <button className="inline-block flex-auto w-[50%] px-[5px] py-[5px] border border-gray-200 rounded-md hover:bg-da-300 hover:text-white">
+                  안했어요
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-[400px] flex justify-center h-[28px] gap-2 mb-[50px]">
+            <ButtonBl>취소</ButtonBl>
+            <ButtonYe>수정완료</ButtonYe>
+          </div>
+        </div>
+      </div>
+
       <Navbar />
     </div>
   );
--- src/pages/UserInfoPage.js
+++ src/pages/UserInfoPage.js
@@ -5,27 +5,24 @@ import Navbar from "../components/Navbar";
 
 function UserInfoPage() {
   return (
-    <div
-      className="w-[500px]  bg-white "
-      style={{ height: "calc(100vh - 65px)" }}
-    >
+    <div className="bg-white ">
       <div className="flex justify-between">
-        <button className=" w-1/2  border-b  border-gray-200 shadow-bottom px-2 py-3 text-[13px] hover:border-gray-800 ">
+        <button className=" w-1/2  border-b  border-gray-200 shadow-bottom px-2 py-3 text-[15px] hover:border-gray-800 ">
           보호자 정보
         </button>
-        <button className="w-1/2 border-b border-gray-200 shadow-bottom px-2 py-3 text-[13px] hover:border-gray-800">
+        <button className="w-1/2 border-b border-gray-200 shadow-bottom px-2 py-3 text-[15px] hover:border-gray-800">
           반려견 정보
         </button>
       </div>
-      {/* content-between h-[670px] */}
-      <div className="w-[350px]  m-auto grid gap-3 pt-[50px] pb-[115px] ">
-        <div className="h-[45px] flex justify-between">
-          <div className="flex justify-start  gap-2">
-            <img
-              src="./images/user-profile.svg"
-              className="w-[20px] h-[20px]"
-            />
-            <h2 className="text-sm">보호자 정보</h2>
+
+      <div
+        className="w-[400px] m-auto grid gap-3 pt-[50px] pb-[115px] "
+        style={{ height: "calc(100vh - 65px)" }}
+      >
+        <div className="h-[45px] flex justify-between p-[10px]">
+          <div className="flex justify-start gap-2">
+            <img src="/images/user-profile.svg" className="w-[20px] h-[20px]" />
+            <h2 className="text-[15px]">보호자 정보</h2>
           </div>
           <button className="px-2 bg-ye-600 w-[55px] h-[25px] text-[11px] text-center rounded-2xl">
             수정하기
@@ -33,7 +30,7 @@ function UserInfoPage() {
         </div>
 
         {/* <div className=" flex-col gap-y-20 "> */}
-        <div className=" nanum flex-col items-center space-y-10 mb-[50px]">
+        <div className="w-[400px] nanum flex-col items-center space-y-10 mb-[50px]">
           <div className="p-2">
             <h2 className="text-base p-[10px]">이름</h2>
             <input
@@ -46,7 +43,7 @@ function UserInfoPage() {
             <h2 className="text-base p-[10px]">이메일</h2>
             <input
               type="text"
-              className="w-[350px] p-[10px] nanum border-b-2 border-da-100 "
+              className="w-full p-[10px] nanum border-b-2 border-da-100 "
             ></input>
           </div>
 
@@ -58,7 +55,7 @@ function UserInfoPage() {
             ></input>
           </div>
 
-          <div className="p-2  \">
+          <div className="p-2">
             <h2 className="text-base  p-[10px] ">주소</h2>
             <input
               type="text"
@@ -66,15 +63,6 @@ function UserInfoPage() {
             ></input>
           </div>
         </div>
-
-        {/* <div className="flex justify-center h-[28px] mb-[50px] gap-2 ">
-          <button className=" w-[82px] text-[13px] text-center rounded-2xl bg-da-300 text-white hover:bg-da-600 hover:text-white">
-            취소
-          </button>
-          <button className=" w-[82px] text-[13px] text-center rounded-2xl bg-ye-600 text-da-300 hover:bg-ye-800 hover:text-da-300">
-            수정완료
-          </button>
-        </div> */}
       </div>
     </div>
   );
