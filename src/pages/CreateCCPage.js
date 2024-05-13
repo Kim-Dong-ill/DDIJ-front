d377531627978ff58096a7175220e4b87753c7ac
chanjin97 <chanjindark@naver.com>
2024-05-13 17:53:31 +0900

    2024.05.13 createCCpage

--- src/pages/CreateCCPage.js
+++ src/pages/CreateCCPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ButtonBl from "../components/ButtonBl";
 import ButtonYe from "../components/ButtonYe";
+import { Link } from "react-router-dom";
 
 const max = [
   { key: 1, value: "2명" },
@@ -39,34 +40,40 @@ function CreateCCPage() {
     setEndshowBox(!endshowBox);
   };
 
-  const handleClick = (buttonName) => {
-    setSelectButton(buttonName);
-  };
+  // const handleClick = (button) => {
+  //   setSelectButton(button);
+  // };
   return (
     <>
       <div
-        className="bg-white px-12 border-2 border-da-100"
+        className="bg-white px-12 border-2 border-da-100 "
         style={{ height: "calc(100%)" }}
       >
-        <div className="pb-[100px]">
+        <div className="pt-[90px] pb-[100px]">
           {/* 경고창 */}
           <div className="mt-4">
-            <div className="text-center  mb-4 px-4 py-2">
+            <div className="text-center border rounded-md  mb-4 px-4 py-2">
               <span className="text-red-500">동일한 시간대</span>에 중복된
               모임이있는지
               <br />
               확인하셨나요?
             </div>
             <div className="flex justify-around items-center mb-4 px-4 py-2 gap-7">
+              {/* onClick 추가 */}
               <button
-                className="border-2 border-da-100 w-[185px] h-[35px] rounded-md hover:bg-da-300 hover:text-wh-100"
-                onClick={() => handleClick("Yesbutton")}
+                className={
+                  "border-2 border-da-100 w-[185px] h-[35px] rounded-md hover:bg-da-300 hover:text-wh-100 ${selectButton === 'Yesbutton'? 'bg-da-300 text-white' : ''}"
+                }
+                // onClick={() => handleClick("Yesbutton")}
               >
                 했어요
               </button>
+              {/* onClick 추가 */}
               <button
-                className="border-2 border-da-100 w-[185px] h-[35px] rounded-md hover:bg-da-100 hover:text-red-500"
-                onClick={() => handleClick("Nobutton")}
+                className={
+                  "border-2 border-da-100 w-[185px] h-[35px] rounded-md hover:bg-da-100 hover:text-red-500 ${selectButton === 'Nobutton' ? 'bg-da-100 text-red-500' : ''}"
+                }
+                // onClick={() => handleClick("Nobutton")}
               >
                 안했어요
               </button>
@@ -92,20 +99,20 @@ function CreateCCPage() {
           {/* 장소,시간설정 */}
           <div>
             <div>
-              <label htmlFor="" className=" mb-4 flex gap-2">
+              <label htmlFor="" className=" mb-4 flex gap-2 ">
                 <img src="/images/plag_icon.svg" alt="깃발아이콘" />
                 출발지
               </label>
               <input
                 type="String"
-                className="w-full border-2 rounded-md mb-3 px-4 py-2"
+                className="w-full border-2 rounded-md mb-3 px-4 py-2 cursor-pointer"
                 onClick={startToggleBox}
                 readOnly
               />
               <img
                 src="/images/plusglass_icon.svg"
                 alt="돋보기아이콘"
-                className="block relative  left-[350px] bottom-[47px]"
+                className="block relative  left-[350px] bottom-[42px] cursor-pointer"
                 onClick={startToggleBox}
               />
               {startshowBox && (
@@ -121,14 +128,14 @@ function CreateCCPage() {
               </h4>
               <input
                 type="text"
-                className="w-full border-2 rounded-md mb-3 px-4 py-2"
+                className="w-full border-2 rounded-md mb-3 px-4 py-2 cursor-pointer"
                 readOnly
                 onClick={endToggleBox}
               />
               <img
                 src="/images/plusglass_icon.svg"
                 alt="돋보기 아이콘"
-                className="block relative  left-[350px] bottom-[47px]"
+                className="block relative  left-[350px] bottom-[42px] cursor-pointer"
                 onClick={endToggleBox}
               />
               {endshowBox && (
@@ -153,27 +160,7 @@ function CreateCCPage() {
                 onChange={startTimeChange}
               />
             </div>
-            <div>
-              <label htmlFor="max" className="block mb-4">
-                <h4 className="flex mb-3 gap-2">
-                  <img src="/images/people_icon.svg" alt="사람 아이콘" />
-                  인원수
-                </h4>
-              </label>
-              <select
-                name="max"
-                id="max"
-                className="w-full px-4 py-2 mb-4 border-2 rounded-md  h-[45px]"
-              >
-                {max.map((item, idx) => {
-                  return (
-                    <option value={item.key} key={idx}>
-                      {item.value}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
+
             <div>
               <label htmlFor="usingTime" className="block mb-4">
                 <h4 className="flex mb-3 gap-2">
@@ -184,9 +171,30 @@ function CreateCCPage() {
               <select
                 name="usingTime"
                 id="usingTime"
-                className="w-full border-2 px-4 py- 2 mb-10 rounded-md block h-[45px]"
+                className="w-full border-2 px-4 py- 2 mb-4 rounded-md block h-[45px] cursor-pointer"
               >
                 {usingTime.map((item, idx) => {
+                  return (
+                    <option value={item.key} key={idx}>
+                      {item.value}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="max" className="block mb-4">
+                <h4 className="flex mb-3 gap-2">
+                  <img src="/images/people_icon.svg" alt="사람 아이콘" />
+                  인원수
+                </h4>
+              </label>
+              <select
+                name="max"
+                id="max"
+                className="w-full px-4 py-2 mb-10 border-2 rounded-md  h-[45px] cursor-pointer"
+              >
+                {max.map((item, idx) => {
                   return (
                     <option value={item.key} key={idx}>
                       {item.value}
@@ -198,8 +206,12 @@ function CreateCCPage() {
           </div>
           {/* 취소,등록버튼 */}
           <div className="flex justify-center items-center gap-10">
-            <ButtonBl className="">취소</ButtonBl>
-            <ButtonYe>등록</ButtonYe>
+            <Link to="/circles">
+              <ButtonBl>취소</ButtonBl>
+            </Link>
+            <Link to="/circles">
+              <ButtonYe>등록</ButtonYe>
+            </Link>
           </div>
         </div>
       </div>
