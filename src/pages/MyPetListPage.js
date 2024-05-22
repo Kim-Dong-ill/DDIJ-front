aa13f2554c89cb46736cfa17f2a087382814d004
ked1125 <ked1125@naver.com>
2024-05-22 15:54:48 +0900

    240522_마이펫리스트수정중_KED

--- src/pages/MyPetListPage.js
+++ src/pages/MyPetListPage.js
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axiosInstance from "../utils/axios";
+import { useSelector } from "react-redux";
 
 function MyPetListPage() {
   const { userId } = useParams();
   const [mypetList, setMyPetList] = useState([]);
   const [petId, setPetId] = useState([]);
-  const [mainPetId, setMainPetId] = useState();
+  // const [mainPetId, setMainPetId] = useState();
   console.log(userId);
   //   useEffect(() => {
   //     const loadPetList = async () => {};
   //     loadPetList();
   //   }, []);
+
+  // 로그인된 유저 _id값 가져오는과정
+  const loginState = useSelector((state) => {
+    return state.user.userData.user.id;
+  });
+  console.log(loginState);
+
   useEffect(() => {
     const loadPetList = async () => {
       try {
         const res = await axiosInstance.get(`/pet/list/${userId}`);
-        console.log(res.data.myPetList);
+        console.log("펫리스트가지금??", res.data.myPetList);
         setMyPetList(res.data.myPetList);
       } catch (error) {}
     };
@@ -31,14 +39,14 @@ function MyPetListPage() {
     }
   }, [mypetList]);
 
-  useEffect(() => {
-    const loadMainPetId = async () => {
-      try {
-        const res = await axiosInstance.get(`/mainpet/${userId}`);
-        setMainPetId(res.data);
-      } catch (error) {}
-    };
-  }, []);
+  // useEffect(() => {
+  //   const loadMainPetId = async () => {
+  //     try {
+  //       const res = await axiosInstance.get(`/mainpet/${userId}`);
+  //       setMainPetId(res.data);
+  //     } catch (error) {}
+  //   };
+  // }, []);
 
   return (
     <div
@@ -50,12 +58,12 @@ function MyPetListPage() {
       })} */}
       {/* 상단 버튼 시작 */}
       <div className="flex mb-[30px]">
-        <Link to="/userinfo/:userid" className="flex-1">
+        <Link to={`/userinfo/${loginState}`} className="flex-1">
           <button className="w-full border-b  border-gray-200 shadow-bottom px-2 py-3 text-[15px] hover:border-gray-800 ">
             보호자 정보
           </button>
         </Link>
-        <Link to="/mypet/:userid" className="flex-1">
+        <Link to={`/mypet/${loginState}`} className="flex-1">
           <button className="w-full  border-b border-gray-200 shadow-bottom px-2 py-3 text-[15px] hover:border-gray-800">
             반려견 정보
           </button>
@@ -70,16 +78,36 @@ function MyPetListPage() {
           <>
             <div className="w-[450px] m-auto grid mb-[25px] bg-white text-center">
               <div className="border border-da-100 rounded-lg">
-                <p className="text-right pr-[15px] pt-[15px]">
-                  {/* {petId==} */}
-                  <button className="bg-ye-600 h-[30px] px-[5px] rounded-lg">
-                    <img
-                      src="/images/star1.svg"
-                      className="inline-block align-middle w-[14px] h-[14px] mr-[3px] "
-                    />
-                    <span className="inline-block  text-[14px]">대표</span>
-                  </button>
-                </p>
+                {/* {petId==} */}
+                {idx == 0 ? (
+                  <p className="text-right pr-[15px] pt-[15px]">
+                    <button className="bg-ye-600 h-[30px] px-[5px] rounded-lg">
+                      <img
+                        src="/images/star1.svg"
+                        className="inline-block align-middle w-[14px] h-[14px] mr-[3px] "
+                      />
+                      <span className="inline-block  text-[14px]">대표</span>
+                    </button>
+                  </p>
+                ) : (
+                  <p className="text-right pr-[15px] pt-[15px]">
+                    <button className="bg-white h-[30px] px-[5px] rounded-lg border border-da-100">
+                      <img
+                        src="/images/star1.svg"
+                        className="inline-block align-middle w-[14px] h-[14px] mr-[3px] "
+                      />
+                      <span className="inline-block  text-[14px]">대표</span>
+                    </button>
+                  </p>
+                )}
+                {/* <button className="bg-ye-600 h-[30px] px-[5px] rounded-lg">
+                  <img
+                    src="/images/star1.svg"
+                    className="inline-block align-middle w-[14px] h-[14px] mr-[3px] "
+                  />
+                  <span className="inline-block  text-[14px]">대표</span>
+                </button> */}
+
                 <img
                   src="/images/dog1.svg"
                   className="w-[100px] h-[100px] rounded-full m-auto"
@@ -98,35 +126,6 @@ function MyPetListPage() {
                   </Link>
                 </div>
               </div>
-
-              {/* <div className="border border-da-100 rounded-lg">
-                <p className="text-right pr-[15px] pt-[15px]">
-                  <button className="bg-white h-[30px] px-[5px] rounded-lg border border-da-100">
-                    <img
-                      src="/images/star1.svg"
-                      className="inline-block align-middle w-[14px] h-[14px] mr-[3px] "
-                    />
-                    <span className="inline-block  text-[14px]">대표</span>
-                  </button>
-                </p>
-                <img
-                  src="/images/dog2.svg"
-                  className="w-[100px] h-[100px] rounded-full m-auto"
-                />
-                <div>
-                  <span className="inline-block leading-[40px] mr-[10px] nanumBold">
-                    나나
-                  </span>
-                  <i class="fa-solid fa-venus"></i>
-                </div>
-                <div className="border-t">
-                  <Link to="/mypet/mod/:petid">
-                    <button className="inilne-block leading-[40px] nanum text-[14px] text-da-500">
-                      수정하기
-                    </button>
-                  </Link>
-                </div>
-              </div> */}
             </div>
           </>
         );
@@ -134,14 +133,14 @@ function MyPetListPage() {
       {mypetList.length < 3 ? (
         <div className="w-[450px] m-auto grid bg-white text-center">
           <div className="border border-da-100 rounded-lg">
-            <p className="text-right pr-[15px] pt-[15px]">
-              <button className="bg-white h-[30px] px-[5px] rounded-lg border border-da-100">
+            <p className="text-right pr-[15px] pt-[45px]">
+              {/* <button className="bg-white h-[30px] px-[5px] rounded-lg border border-da-100">
                 <img
                   src="/images/star1.svg"
                   className="inline-block align-middle w-[14px] h-[14px] mr-[3px] "
                 />
                 <span className="inline-block  text-[14px]">대표</span>
-              </button>
+              </button> */}
             </p>
             <div className=" bg-ye-500 h-[100px] w-[100px] rounded-full flex justify-center items-center m-auto ">
               <img
