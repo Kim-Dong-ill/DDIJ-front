ed48939e8b32351c14859e7884feb9508a67b553
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-05-30 12:12:00 +0900

    모임생성 정확한 좌표 얻기

--- src/kakaoMap/Kakao_start_point.js
+++ src/kakaoMap/Kakao_start_point.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-function Kakao_start_point({ startCoord, startToggleBox }) {
+function Kakao_start_point({ startCoord, startToggleBox, handleDragLoc }) {
   const { kakao } = window;
   const [map, setMap] = useState(null); // 카카오 map
 
@@ -23,14 +23,25 @@ function Kakao_start_point({ startCoord, startToggleBox }) {
       // 마우스 드래그로 지도 이동이 완료되었을 때 호출
       kakao.maps.event.addListener(newMap, "dragend", function () {
         const latlng = newMap.getCenter();
-        console.log("출발지 좌표", latlng);
+        // console.log("출발지 좌표", latlng);
+        const coordinates = [Number(latlng.La), Number(latlng.Ma)];
+        console.log("출발지 좌표", coordinates);
+        handleDragLoc(coordinates);
       });
     }
   }, [startCoord]);
 
   return (
     <div>
-      <div id="startMap" style={{ width: "100%", height: "400px" }}></div>
+      <div
+        className="relative"
+        id="startMap"
+        style={{ width: "100%", height: "400px" }}
+      >
+        <div className="endMarker z-50">
+          <img className="w-[35px]" src="./images/marker.svg" alt="" />
+        </div>
+      </div>
       <div
         onClick={startToggleBox}
         className="result p-2 border border-da-200 rounded-md m-2 text-center"
